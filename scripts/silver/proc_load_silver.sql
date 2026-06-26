1e020b13a45d372a6371fffe7ca3419fbc9c5d4d
jaiviswan <jaiviswan@gmail.com>
2026-06-26 13:01:55 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -20,3 +20,32 @@ ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_la
 from bronze.crm_cust_info
 )t
 WHERE flag_last = 1
+
+
+insert into silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+Select 	
+	prd_id,
+	REPLACE(SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+	SUBSTRING(prd_key,7,LEN(prd_key)) as prd_key,
+	prd_nm,
+	ISNULL(prd_cost,0) as prd_cost,
+	CASE UPPER(TRIM(prd_line)) 
+		 WHEN 'M' THEN 'Mountain'
+		 WHEN 'R' THEN 'Road'
+		 WHEN 'S' THEN 'Other Sales'
+		 WHEN 'T' THEN 'Touring'
+		 ELSE 'n/a'
+END prd_line,
+CAST(prd_start_dt AS DATE) as prd_start_dt,
+CAST(DATEADD(day,-1,LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)) as DATE) as prd_end_dt_test
+From bronze.crm_prd_info
+
