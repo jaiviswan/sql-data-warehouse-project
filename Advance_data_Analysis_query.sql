4b3aec01e0b51f7b299ef9be920544bc41bbf53d
jaiviswan <jaiviswan@gmail.com>
2026-07-01 00:34:17 +0530

    Update Advance_data_Analysis_query.sql

--- Advance_data_Analysis_query.sql
+++ Advance_data_Analysis_query.sql
@@ -119,3 +119,81 @@ END Customer_segment
 From customer_spending)t
 GROUP BY Customer_segment
 ORDER BY Total_customers DESC
+
+--FINAL Report (gold.report_customers)
+-- this is base query using CTE to use for aggregation
+CREATE VIEW gold.report_customers as
+WITH base_query as
+(
+SELECT 
+f.order_number,
+f.product_key,
+f.order_date,
+f.sales_amount,
+f.quantity,
+c.customer_key,
+c.customer_nuumber,
+CONCAT(c.first_name, ' ', c.last_name) as customer_name,
+DATEDIFF(year,c.birthdate, GETDATE()) age
+from gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_id
+WHERE order_date IS NOT NULL AND customer_key IS NOT NULL )
+
+--this is customer aggreation details
+, customer_aggregation as (
+Select 
+	customer_key,
+	customer_nuumber,
+	customer_name,
+	age,
+	COUNT(DISTINCT order_number) as Total_orders,
+	SUM(sales_amount) as Total_sales,
+	SUM(quantity) as Total_Quantity,
+	COUNT(product_key) as Total_product,
+	MAX(order_date) as Last_order,
+	DATEDIFF(month,MIN(order_date),MAX(order_date)) as Life_span
+From base_query
+GROUP BY
+	customer_key,
+	customer_nuumber,
+	customer_name,
+	age
+)
+
+--Final query
+select 
+customer_key,
+customer_nuumber,
+customer_name,
+age,
+CASE 
+	 WHEN age < 20 THEN 'Under 20'
+	 WHEN age between 20 and 29 THEN '20-29'
+	 WHEN age between 30 and 39 THEN '30-39'
+	 WHEN age between 40 and 49 THEN '40-49'
+	 ELSE '50 and above'
+END age_group,
+CASE 
+	 WHEN Life_Span >=12 AND Total_sales >5000 THEN 'VIP'
+	 WHEN Life_Span >=12 AND Total_sales <5000 THEN 'Regular'
+	 ELSE 'New'
+END customer_segment,
+Total_orders,
+Total_sales,
+Total_Quantity,
+Total_product,
+Last_order,
+DATEDIFF(MONTH,last_order,GETDATE()) as recency,
+Life_span,
+--Computation of avg order value 
+CASE 
+	WHEN Total_sales = 0 THEN 0
+	ELSE Total_sales/Total_orders
+ END avg_order_value,
+
+ --Compute avgerage monthly spend
+CASE WHEN Life_span = 0 THEN Total_sales
+	 ELSE Total_sales/Life_span
+END avg_spend
+from customer_aggregation
